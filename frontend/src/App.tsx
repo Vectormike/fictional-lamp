04bd8353234e9723d35b9ce0470a56e7dc152088
Victor Jonah <victorjonah199@gmail.com>
2025-01-29 07:07:53 +0100

    add App component and corresponding tests for superhero manager

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SuperheroManager from './components/SuperheroManager';
+
+function App() {
+  return (
+    <div className='App'>
+      <SuperheroManager />
+    </div>
+  );
+}
+
+export default App;
